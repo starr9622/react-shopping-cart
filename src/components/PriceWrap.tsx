6a6130999511d6b901b6bcba5cfcb0b579ca6372
starr9622 <sbseodev@gmail.com>
2020-11-17 15:17:40 +0900

    결제금액 글자 스타일 변경

--- src/components/PriceWrap.tsx
+++ src/components/PriceWrap.tsx
@@ -3,9 +3,15 @@ import NumberWrap from './NumberWrap';
 import { PropsPriceWrap } from '../types';
 
 export default function PriceWrap(props: PropsPriceWrap) {
+  const className =
+    props.title.indexOf('할인') !== -1
+      ? 'priceWrap discount'
+      : props.title.indexOf('결제') !== -1
+      ? 'priceWrap payment'
+      : 'priceWrap';
   return (
     <>
-      <div className="priceWrap">
+      <div className={className}>
         <span>{props.title}</span>
         <span>
           <NumberWrap num={props.price} />원
